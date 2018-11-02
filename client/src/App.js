8e0b095779af016c041199f005781f866a9c9c9e
jessejames black <jessejames.black@infsys.com>
2018-11-02 11:27:46 -0400

    routes for login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
-import React, {
-  Component
-} from 'react';
-import NavBar from './components/layout/NavBar';
-import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import NavBar from './components/layouts/NavBar';
+import Footer from './components/layouts/Footer';
+import Landing from './components/layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar></NavBar>
-          <Landing></Landing>
-        <Footer></Footer>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar></NavBar>
+          <Route exact path="/" component={Landing}></Route>
+          <div className="container">
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </div>
+          <Footer></Footer>
+        </div>
+      </Router>
     );
   }
 }
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://jesse:jessej1@ds033187.mlab.com:33187/mernproject2018",
+  //mongoURI: "mongodb://jesse:jessej1@ds033187.mlab.com:33187/mernproject2018",
+  mongoURI: "mongodb://localhost:27017/mernProject",
   secretOrKey: 'secret'
 }
