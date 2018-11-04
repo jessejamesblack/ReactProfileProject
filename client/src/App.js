ad659147705ce1d2c4ee98ee061c206638cfc6bc
jessejamesblack <jessejamesblack@optonline.net>
2018-11-03 21:06:03 -0400

    start of redux implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import NavBar from './components/layout/NavBar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import store from './store';
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <NavBar></NavBar>
-          <Route exact path="/" component={Landing}></Route>
-          <div className="container">
-            <Route exact path="/register" component={Register}></Route>
-            <Route exact path="/login" component={Login}></Route>
+      <Provider>
+        <Router>
+          <div className="App">
+            <NavBar></NavBar>
+            <Route exact path="/" component={Landing}></Route>
+            <div className="container">
+              <Route exact path="/register" component={Register}></Route>
+              <Route exact path="/login" component={Login}></Route>
+            </div>
+            <Footer></Footer>
           </div>
-          <Footer></Footer>
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
