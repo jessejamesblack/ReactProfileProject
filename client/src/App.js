345eae4d6b41f432c48d3f3f3e3ac32a3a3ff531
jessejamesblack <jessejamesblack@optonline.net>
2018-11-04 19:14:30 -0500

    map state to props

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Provider>
+      <Provider store={store}>
         <Router>
           <div className="App">
             <NavBar></NavBar>
