235a2b57530e7ead98f07d0de47179ec6d1b6a9d
jessejamesblack <jessejamesblack@optonline.net>
2018-11-06 18:10:29 -0500

    login user redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import NavBar from './components/layout/NavBar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -10,6 +12,16 @@ import Login from './components/auth/Login';
 import store from './store';
 
 import './App.css';
+
+// Check for token
+if(localStorage.jwtToken){
+   // set auth token header auth
+   setAuthToken(localStorage.jwtToken);
+   // decode token and get user info and experation
+   const decoded = jwt_decode(localStorage.jwtToken);
+   // set user and isAuthenticated
+   store.dispatch(setCurrentUser(decoded));
+}
 
 
 class App extends Component {
